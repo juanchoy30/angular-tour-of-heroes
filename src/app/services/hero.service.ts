4e93a1d23a00c01d512d78c4c64dbda3af1b11b6
Juan Miguel Choy <juanchoy30@gmail.com>
2020-05-11 10:50:21 -0300

    add heroes

--- src/app/services/hero.service.ts
+++ src/app/services/hero.service.ts
@@ -73,4 +73,13 @@ private handleError<T>(operation = 'operation', result?: T) {
     );
   }
 
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
+  
+
 }
